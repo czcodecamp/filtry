efb56710b9c046eb40aeb3e5e0bb5adee04bc142
Aleku22 <a.kudela@seznam.cz>
2016-11-27 19:57:12 +0100

    + aktulizovana metoda findByCategoryAndFilter() 	- vyhledavani dle filterType parametru

--- src/AppBundle/Facade/ProductFacade.php
+++ src/AppBundle/Facade/ProductFacade.php
@@ -27,28 +27,29 @@ class ProductFacade {
 	}
 	
 	public function findByCategoryAndFilter(Category $category, $filter, $limit, $offset) {
-		$query =  $this->productRepository->findByCategoryAndFilter($category);
-
+		$query =  $this->productRepository->findByCategory($category);
 		
 		foreach ($filter as $k => $v) {
 			$param = $v['param'];
 			$values = $v['values']; 
+						
+			$query->leftJoin('AppBundle\Entity\ProductParameter', 'pp'.$k.'', 'WITH', 'p = pp'.$k.'.product')
+			->leftJoin('AppBundle\Entity\Parameter', 'param'.$k.'', 'WITH', 'param'.$k.' = pp'.$k.'.parameter');
 			
 			$dataType = ucfirst($param->getDataType());
 			if($param->getFilterType() == 'multiselect') {
 				$values = explode(',', $values);	
-				$query->andWhere('pp.value'.$dataType.' IN (:values'.$k.')')
+				$query->andWhere('pp'.$k.'.value'.$dataType.' IN (:values'.$k.')')
 				->setParameter('values'.$k.'', $values);			
-			}/* elseif($param->getFilterType() == 'yesno') {
-				$query->where(':column'.$k.' == (:values'.$k.')')
-				->setParameter('column'.$k, 'value_' . $param->getDataType())
-				->setParameter('values'.$k, $values);			
+			} elseif($param->getFilterType() == 'yesno') {
+				$query->andWhere('pp'.$k.'.value'.$dataType.' = (:values'.$k.')')
+				->setParameter('values'.$k, (int)$values);			
 			} elseif($param->getFilterType() == 'range') {
-				$query->where(':column'.$k.' >= :min'.$k.' and :column'.$k.' <= :max'.$k.'')
-				->setParameter('column'.$k, 'value_' . $param->getDataType())
+				$values = explode(',', $values);
+				$query->andWhere('pp'.$k.'.value'.$dataType.' >= :min'.$k.' and pp'.$k.'.value'.$dataType.' <= :max'.$k)
 				->setParameter('min'.$k, $values[0])
 				->setParameter('max'.$k, $values[1]);			
-			}*/	
+			}	
 		}
 			$query->setFirstResult($offset)
 			->setMaxResults($limit)->groupBy('p');
